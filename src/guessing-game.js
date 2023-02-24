270730f13e5fe0d4a50f00445651ca34d031ec04
Qwerton <Qwerton@users.noreply.github.com>
2023-02-24 12:01:50 +0300

    result

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this.low = min;
+      this.high = max;
     }
 
     guess() {
-
+        this.mid = Math.ceil((this.high + this.low) / 2);
+      return this.mid;
     }
 
     lower() {
-
+      this.high = this.mid;
     }
 
     greater() {
-
+      this.low = this.mid;
     }
 }
 
